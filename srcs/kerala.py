760fbc5ae016a44c90ef4e67aab93cb6cae32645
ramSeraph <kandimalla.sreeram@gmail.com>
2025-03-24 18:57:24 +0530

    use the proper key for partnum, rewire a little to make it more readable, remove some redunndant code, more documentation of data seen

--- srcs/kerala.py
+++ srcs/kerala.py
@@ -24,12 +24,39 @@ def tokenize(number):
         remainder = math.floor(float(remainder) / float(64))
     return token
 
-#{<class 'calmjs.parse.asttypes.String'>: ['allowScriptTagRemoting is false.'], <class 'calmjs.parse.asttypes.FunctionCall'>: [[[[], {'r': 'window.dwr._[0]', <class 'calmjs.parse.asttypes.FunctionCall'>: [['r.handleCallback', ['0', '0', 'mBYbyy7ZXfr7Nx2Au1R$1bKww2p']]]}], []]]}
 def get_callback_args(ast_dict):
     try:
         return ast_dict[FunctionCall][0][0][1][FunctionCall][0][1]
     except Exception:
         return None
+
+#   
+#   sample js 1
+#
+#   (function(){
+#   var r=window.dwr._[0];
+#   //#DWR-INSERT
+#   //#DWR-REPLY
+#   r.handleCallback("0","0","JSa6zBjjWGqV55HYHx98D$uj9np");
+#   })();
+#   
+#   sample ast dict 1
+#
+#   {<class 'calmjs.parse.asttypes.String'>: ['allowScriptTagRemoting is false.'], <class 'calmjs.parse.asttypes.FunctionCall'>: [[[[], {'r': 'window.dwr._[0]', <class 'calmjs.parse.asttypes.FunctionCall'>: [['r.handleCallback', ['0', '0', 'JSa6zBjjWGqV55HYHx98D$uj9np']]]}], []]]}
+
+#   sample js 2
+#
+#   throw 'allowScriptTagRemoting is false.';
+#   (function(){
+#   var r=window.dwr._[0];
+#   //#DWR-INSERT
+#   //#DWR-REPLY
+#   r.handleCallback("0","0",[[null,1122,2025,38687,"Vigilance",new Date(1742754600000),"Vigilance(C)","Select List of Legal Advisers in the Vigilance & Anti Corruption Bureau"],[null,1123,2025,38688,"Home ",new Date(1742754600000),"Home(M)","According consent to CBI investigation"],[null,1124,2025,38689,"Revenue Department",new Date(1742754600000),"O/o Special Tahsildar(LA)  NoII  Kochi Metro Rail Project","Chilavannur Canal Development (IURWTS Project)"],[null,1125,2025,38690,"Revenue Department",new Date(1742754600000),"Collectorate Kottayam","Fair Value of the land in Block 05 Survey No. 256/10 of Vazhappally west Village in Changanassery Taluk of Kottayam District"],[null,1126,2025,38691,"Revenue Department",new Date(1742754600000),"O/o Special Tahsildar(LA)  NoII  Kochi Metro Rail Project","Infopark Metro Station Construction"],[null,1127,2025,38692,"Revenue Department",new Date(1742754600000),"Collectorate Kottayam","Fair Value of the land in Block 12 Survey No. 89/2 of Kanjirappally Village in Kanjirappally Taluk of Kottayam District"],[null,1128,2025,38693,"Revenue Department",new Date(1742754600000),"COLLECTORATE  ERNAKULAM","DCEKM/7928/2023-C7"],[null,1129,2025,38694,"Revenue Department",new Date(1742754600000),"Collectorate Kottayam","Fair Value of the land in Block   24 Survey No. 102/32 of Erumely South Village in Kanjirapally Taluk of Kottayam District"],[null,1130,2025,38695,"Revenue Department",new Date(1742754600000),"Collectorate Kottayam","Fair Value of the land in Survey No 463/1B of Vechoor Village in Vaikom Taluk of Kottayam District"],[null,1131,2025,38696,"Revenue Department",new Date(1742754600000),"COLLECTORATE  THIRUVANANTHAPURAM","Land Aacquisition - Thoduve Bridge and Approach Road 4(1) notification"],[null,1132,2025,38697,"Revenue Department",new Date(1742754600000),"Collectorate Kottayam","Fair Value of the land in Block   31 Survey No. 110/9 of Muttuchira Village in Vaikom Taluk of Kottayam District"],[null,1133,2025,38698,"Revenue Department",new Date(1742754600000),"Collectorate Kottayam","Fair Value of the land in Block 17 Survey No. 238/5-2 of Thiruvarppu Village in Kottayam Taluk of Kottayam District"],[null,1134,2025,38699,"Revenue Department",new Date(1742754600000),"Revenue(B)","Development of Sasthamangalam - Mannarakonam Road (Reach 1)"],[null,1135,2025,38700,"Home ",new Date(1742754600000),"Home(C)","conversion of six erstwhile  Mobile Court-"]]);
+#   })();
+#   
+#   sample ast dict 2
+#
+#   {<class 'calmjs.parse.asttypes.String'>: ['allowScriptTagRemoting is false.'], <class 'calmjs.parse.asttypes.FunctionCall'>: [[[[], {'r': 'window.dwr._[0]', <class 'calmjs.parse.asttypes.FunctionCall'>: [['r.handleCallback', ['0', '0', [[None, 1122, 2025, 38687, 'Vigilance', ['Date', [1742754600000]], 'Vigilance(C)', 'Select List of Legal Advisers in the Vigilance & Anti Corruption Bureau'], [None, 1123, 2025, 38688, 'Home ', ['Date', [1742754600000]], 'Home(M)', 'According consent to CBI investigation'], [None, 1124, 2025, 38689, 'Revenue Department', ['Date', [1742754600000]], 'O/o Special Tahsildar(LA)  NoII  Kochi Metro Rail Project', 'Chilavannur Canal Development (IURWTS Project)'], [None, 1125, 2025, 38690, 'Revenue Department', ['Date', [1742754600000]], 'Collectorate Kottayam', 'Fair Value of the land in Block 05 Survey No. 256/10 of Vazhappally west Village in Changanassery Taluk of Kottayam District'], [None, 1126, 2025, 38691, 'Revenue Department', ['Date', [1742754600000]], 'O/o Special Tahsildar(LA)  NoII  Kochi Metro Rail Project', 'Infopark Metro Station Construction'], [None, 1127, 2025, 38692, 'Revenue Department', ['Date', [1742754600000]], 'Collectorate Kottayam', 'Fair Value of the land in Block 12 Survey No. 89/2 of Kanjirappally Village in Kanjirappally Taluk of Kottayam District'], [None, 1128, 2025, 38693, 'Revenue Department', ['Date', [1742754600000]], 'COLLECTORATE  ERNAKULAM', 'DCEKM/7928/2023-C7'], [None, 1129, 2025, 38694, 'Revenue Department', ['Date', [1742754600000]], 'Collectorate Kottayam', 'Fair Value of the land in Block   24 Survey No. 102/32 of Erumely South Village in Kanjirapally Taluk of Kottayam District'], [None, 1130, 2025, 38695, 'Revenue Department', ['Date', [1742754600000]], 'Collectorate Kottayam', 'Fair Value of the land in Survey No 463/1B of Vechoor Village in Vaikom Taluk of Kottayam District'], [None, 1131, 2025, 38696, 'Revenue Department', ['Date', [1742754600000]], 'COLLECTORATE  THIRUVANANTHAPURAM', 'Land Aacquisition - Thoduve Bridge and Approach Road 4(1) notification'], [None, 1132, 2025, 38697, 'Revenue Department', ['Date', [1742754600000]], 'Collectorate Kottayam', 'Fair Value of the land in Block   31 Survey No. 110/9 of Muttuchira Village in Vaikom Taluk of Kottayam District'], [None, 1133, 2025, 38698, 'Revenue Department', ['Date', [1742754600000]], 'Collectorate Kottayam', 'Fair Value of the land in Block 17 Survey No. 238/5-2 of Thiruvarppu Village in Kottayam Taluk of Kottayam District'], [None, 1134, 2025, 38699, 'Revenue Department', ['Date', [1742754600000]], 'Revenue(B)', 'Development of Sasthamangalam - Mannarakonam Road (Reach 1)'], [None, 1135, 2025, 38700, 'Home ', ['Date', [1742754600000]], 'Home(C)', 'conversion of six erstwhile  Mobile Court-']]]]]}], []]]}
 
 class KeralaCompose(BaseGazette):
     def __init__(self, name, storage):
@@ -39,19 +66,36 @@ class KeralaCompose(BaseGazette):
         self.searchurl  = 'https://compose.kerala.gov.in/egazettelink1'
         self.hostname   = 'compose.kerala.gov.in'
 
-    def make_dwr_call(self, script_name, method_name, cookiejar, curr_url, dateobj, \
-                      sessionid = None, need_session = True, \
-                      batchid = 0, extra_args = {}):
-        if sessionid is None and need_session:
-            self.logger.error('sessionid not set while making a dwr call to %s.%s for %s', \
-                              script_name, method_name, dateobj) 
+    def parse_js_response(self, js_txt, dateobj):
+
+        try:
+            js_resp = es5(js_txt)
+        except Exception:
+            self.logger.warning('Unable to parse response as js for %s', dateobj)
             return None
+
+        ast_dict = ast_to_dict(js_resp)
+
+        callback_args = get_callback_args(ast_dict)
+        if callback_args is None:
+            self.logger.warning('Unable to get callback arguments for %s', dateobj)
+            return None
+
+        if len(callback_args) != 3:
+            self.logger.warning('Unexpected callback arguments for %s', dateobj)
+            return None
+
+        return callback_args[2]
+
+
+    def make_dwr_call(self, script_name, method_name, cookiejar, curr_url, dateobj, \
+                      sessionid = None, batchid = 0, extra_args = {}):
 
         curr_page = urllib.parse.urlparse(curr_url).path
         curr_page = urllib.parse.quote_plus(curr_page)
         
         dwr_path = f'/dwr/call/plaincall/{script_name}.{method_name}.dwr'
-        dwr_url = urllib.parse.urljoin(self.baseurl, dwr_path)
+        dwr_url  = urllib.parse.urljoin(self.baseurl, dwr_path)
         
         sessionid_str = ''
         if sessionid is not None:
@@ -60,15 +104,15 @@ class KeralaCompose(BaseGazette):
             sessionid_str = f'{sessionid}/{tokenize(epochtime)}-{tokenize(rand)}'
 
         post_data = {
-            'callCount': '1',
-            'windowName': '',
-            'c0-scriptName': script_name,
-            'c0-methodName': method_name,
-            'c0-id': '0',
-            'batchId': batchid,
-            'page': curr_page,
-            'instanceId': '0',
-            'scriptSessionId': sessionid_str
+            'callCount'       : '1',
+            'windowName'      : '',
+            'c0-scriptName'   : script_name,
+            'c0-methodName'   : method_name,
+            'c0-id'           : '0',
+            'batchId'         : batchid,
+            'page'            : curr_page,
+            'instanceId'      : '0',
+            'scriptSessionId' : sessionid_str
         }
         post_data.update(extra_args)
 
@@ -76,7 +120,7 @@ class KeralaCompose(BaseGazette):
         for k,v in post_data.items():
             post_data_strs.append(f'{k}={v}')
 
-        post_data_full_str = '\n'.join(post_data_strs)
+        post_data_full_str   = '\n'.join(post_data_strs)
         post_data_full_bytes = post_data_full_str.encode('utf-8')
         
         headers = { 'Content-Type': 'text/plain' }
@@ -87,22 +131,7 @@ class KeralaCompose(BaseGazette):
                                 script_name, method_name, dateobj)
             return None
 
-        txt = response.webpage.decode('utf-8')
-        try:
-            js_resp = es5(txt)
-        except Exception:
-            self.logger.warning('Unable to parse response as js for %s', dateobj)
-            return None
-        js_dict = ast_to_dict(js_resp)
-        callback_args = get_callback_args(js_dict)
-        if callback_args is None:
-            self.logger.warning('Unable to get callback arguments for %s', dateobj)
-            return None
-
-        if len(callback_args) != 3:
-            self.logger.warning('Unexpected callback arguments for %s', dateobj)
-            return None
-        return callback_args[2]
+        return self.parse_js_response(response.webpage.decode('utf-8'), dateobj)
 
     def get_dwr_post_args_extraordinary(self, dateobj):
         datestr = dateobj.strftime('%Y-%m-%d')
@@ -176,12 +205,12 @@ class KeralaCompose(BaseGazette):
             metainfo = utils.MetaInfo()
             metainfo.set_date(dateobj)
 
-            metainfo['gztype'] = 'Extraordinary'
-            metainfo['gznum'] = result[1]
+            metainfo['gztype']     = 'Extraordinary'
+            metainfo['gznum']      = result[1]
             metainfo['department'] = result[4]
-            metainfo['office'] = result[6]
-            metainfo['subject'] = result[7]
-            metainfo['num'] = result[3]
+            metainfo['office']     = result[6]
+            metainfo['subject']    = result[7]
+            metainfo['num']        = result[3]
 
             minfos.append(metainfo)
 
@@ -198,11 +227,11 @@ class KeralaCompose(BaseGazette):
             metainfo = utils.MetaInfo()
             metainfo.set_date(dateobj)
 
-            metainfo['gztype'] = 'Ordinary'
-            metainfo['part'] = result[0]
-            metainfo['gznum'] = result[1]
+            metainfo['gztype']     = 'Ordinary'
+            metainfo['partnum']    = result[0]
+            metainfo['gznum']      = result[1]
             metainfo['department'] = result[4]
-            metainfo['num'] = result[3]
+            metainfo['num']        = result[3]
 
             minfos.append(metainfo)
 
@@ -210,19 +239,21 @@ class KeralaCompose(BaseGazette):
 
     def download_onetype(self, relpath, dateobj, gztype):
         dls = []
-
         cookiejar = CookieJar()
+
         response = self.download_url(self.baseurl, savecookies = cookiejar)
         if not response or not response.webpage:
             self.logger.warning('Unable to get base page for day: %s', dateobj)
             return dls
+
         curr_url = response.response_url
 
-        dwr_sessionid = self.make_dwr_call('__System', 'generateId', cookiejar, curr_url, dateobj, \
-                                           need_session = False)
+        dwr_sessionid = self.make_dwr_call('__System', 'generateId', cookiejar, curr_url, dateobj)
+
         cookie = Cookie(0, 'DWRSESSIONID', dwr_sessionid, None, False, \
                         self.hostname, True, False, '/', True, False, \
                         None, False, None, None, None)
+
         cookiejar.set_cookie(cookie) 
 
         response = self.download_url(self.searchurl, savecookies = cookiejar, \
@@ -230,6 +261,7 @@ class KeralaCompose(BaseGazette):
         if not response or not response.webpage:
             self.logger.warning('Unable to get search page for day: %s', dateobj)
             return dls
+
         curr_url = response.response_url
 
         if gztype == 'Extraordinary':
@@ -237,10 +269,12 @@ class KeralaCompose(BaseGazette):
         else:
             postargs = self.get_dwr_post_args_ordinary(dateobj)
 
+
         results = self.make_dwr_call('gazetteUploadDAO', 'getgazettesearchData', cookiejar, curr_url, dateobj, \
-                                     sessionid = dwr_sessionid, need_session = False, extra_args = postargs)
+                                     sessionid = dwr_sessionid, extra_args = postargs)
         if results is None:
             return dls
+
 
         formdata = self.get_download_form_data(response.webpage)
         if formdata is None:
@@ -252,19 +286,21 @@ class KeralaCompose(BaseGazette):
         else:
             metainfos = self.get_metainfos_ordinary(results, dateobj)
 
+
         for metainfo in metainfos:
             num = metainfo.pop('num')
 
             formdata = utils.replace_field(formdata, 'searchpdfid', num)
+
             query_str = urllib.parse.urlencode(formdata).encode('utf-8')
+
             url = urllib.parse.urljoin(self.baseurl, 'kgSearchfiledownloadpdf')
             url = f'{url}?{query_str}'
 
-            metainfo['url'] = url
-
             gztype = metainfo['gztype'].lower()
             relurl = os.path.join(relpath, f'{gztype}-{num}')   
-            if self.save_gazette(relurl, metainfo['url'], metainfo, cookiefile = cookiejar):
+
+            if self.save_gazette(relurl, url, metainfo, cookiefile = cookiejar):
                 dls.append(relurl)
 
         return dls
